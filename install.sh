1f0ed5f8d765543c197c2738d1c2a31ea88cbf34
Anh DH <anhdhbn@gmail.com>
2020-03-22 02:46:14 +0700

    kind

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 # /bin/bash
-sudo apt-get update
+SWAP_FOLDER="/swapfile"
+if grep -Fxq $SWAP_FOLDER /etc/fstab
+then
+  echo "$SWAP_FOLDER was swaped"
+else
+  sudo fallocate -l 4G $SWAP_FOLDER
+  sudo chmod 600 $SWAP_FOLDER
+  sudo mkswap $SWAP_FOLDER
+  sudo swapon $SWAP_FOLDER
+  echo "$SWAP_FOLDER none swap sw 0 0" | sudo tee -a /etc/fstab
+fi
 
 # install docker
+sudo apt-get update
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
 
-# install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
-  && chmod +x minikube
-sudo mkdir -p /usr/local/bin/
-sudo install minikube /usr/local/bin/
+curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-$(uname)-amd64"
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
 
-# install kubectl
-sudo apt-get update && sudo apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-
-# run minikube
-sudo minikube start --driver=none --v=7 --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --cpus 1
-
-
+kind create cluster
