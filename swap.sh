ea8cc3cbfd61382027898af84cd0b96c6cf9adf0
Anh DH <anhdhbn@gmail.com>
2020-03-22 01:40:57 +0700

    init

--- swap.sh
+++ swap.sh
@@ -3,10 +3,10 @@ if grep -Fxq "$SWAP_FOLDER" /etc/fstab
 then
     echo "swaped"
 else
-    sudo fallocate -l 4G $SWAP_FOLDER
-    sudo chmod 600 $SWAP_FOLDER
-    sudo mkswap $SWAP_FOLDER
-    sudo swapon $SWAP_FOLDER
+    sudo fallocate -l 4G "$SWAP_FOLDER"
+    sudo chmod 600 "$SWAP_FOLDER"
+    sudo mkswap "$SWAP_FOLDER"
+    sudo swapon "$SWAP_FOLDER"
     sudo bash -c 'echo "$SWAP_FOLDER none swap sw 0 0" >> /etc/fstab'
     sudo reboot
 fi
