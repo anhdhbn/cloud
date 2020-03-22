75cc0534e43bf58462c2230ea5f76afcc5b693a1
Anh DH <anhdhbn@gmail.com>
2020-03-22 11:37:57 +0700

    docker

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 # /bin/bash
+sudo apt-get update
+sudo apt install -y software-properties-common
+
+# swap 
 SWAP_FOLDER="/swapfile"
 if grep -q "$SWAP_FOLDER" /etc/fstab
 then
@@ -15,9 +19,14 @@ fi
 if docker --version | grep "Docker version"
 then
   echo "Docker was installed"
+  # sudo apt-get remove docker docker-engine docker.io
 else
   sudo apt-get update
-  sudo apt install -y docker.io 
+  sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce
   sudo systemctl start docker
   sudo systemctl enable docker
   sudo groupadd docker
@@ -40,7 +49,6 @@ fi
 
 # install virtualbox
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo apt install -y software-properties-common
 sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
 sudo apt update && sudo apt install -y virtualbox-6.0
 
