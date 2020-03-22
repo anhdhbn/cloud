864a857fd7de541314718c9747f6466500179e6c
Anh DH <anhdhbn@gmail.com>
2020-03-22 13:38:19 +0700

    hide virtualbox

--- install.sh
+++ install.sh
@@ -48,13 +48,13 @@ else
 fi
 
 # install virtualbox
-DEB_VIRTUALBOX="deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo add-apt-repository $DEB_VIRTUALBOX
-if ! grep -q "$DEB_VIRTUALBOX" /etc/apt/sources.list ; then
-  sudo add-apt-repository "$DEB_VIRTUALBOX"
-fi
-sudo apt update && sudo apt install -y virtualbox-6.0
+# DEB_VIRTUALBOX="deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+# wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+# sudo add-apt-repository $DEB_VIRTUALBOX
+# if ! grep -q "$DEB_VIRTUALBOX" /etc/apt/sources.list ; then
+#   sudo add-apt-repository "$DEB_VIRTUALBOX"
+# fi
+# sudo apt update && sudo apt install -y virtualbox-6.0
 
 # install minikube
 curl -Lo minikube "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64" \
