f3f6d93991c02812dff914ce012f2b6ac9c89a5e
Anh DH <anhdhbn@gmail.com>
2020-03-22 23:05:41 +0700

    init

--- docker.sh
+++ docker.sh
@@ -34,15 +34,6 @@ sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
 
 
-# install virtualbox
-DEB_VIRTUALBOX="deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian bionic contrib"
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-sudo add-apt-repository $DEB_VIRTUALBOX
-if ! grep -q "$DEB_VIRTUALBOX" /etc/apt/sources.list ; then
-  sudo add-apt-repository "$DEB_VIRTUALBOX"
-fi
-sudo apt update && sudo apt install -y virtualbox-6.0
-
 newgrp docker << EOF
-sudo minikube start  --v=7 --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --no-vtx-check --wait=false --driver=virtualbox --cpus=1
+sudo minikube start  --v=7 --extra-config=kubeadm.ignore-preflight-errors=NumCPU --force --no-vtx-check --wait=false --driver=docker --cpus=1
 EOF
