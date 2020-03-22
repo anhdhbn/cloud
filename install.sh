55fcdcb98958becf67ade8b06be7b6a4bb7dfc57
Anh DH <anhdhbn@gmail.com>
2020-03-22 19:43:11 +0700

    fix docker

--- install.sh
+++ install.sh
@@ -18,7 +18,6 @@ sudo apt install -y software-properties-common
 # install docker $(lsb_release -cs)
 DEB_DOCKER="deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get remove -y docker* containerd*
-sudo apt autoremove -y
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
